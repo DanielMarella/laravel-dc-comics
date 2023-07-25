9d1d5e28f0a9400c74eb587918413675fd6f3e15
DanielMarella <marella.daniel96@gmail.com>
2023-07-25 18:41:11 +0200

    create a new form page for insert the new element

---
+++ resources/views/admin/comics/create.blade.php
@@ -0,0 +1,79 @@
+@extends('layouts.app')
+
+@section('title', 'Create Comic Page')
+
+@section('main-content')
+
+
+
+<div class="container">
+    <h1 class="mb-3">
+        Add A New Comic
+    </h1>
+    <div class="row">
+        <div class="card col-12" >
+            <form>
+                <div class="mb-3">
+                    <label for="title" class="form-label">
+                        Title
+                    </label>
+                    <input type="text" class="form-control" id="title" name="title">
+                </div>
+                <div class="mb-3">
+                    <label for="description" class="form-label">
+                        Description
+                    </label>
+                    <input type="text" class="form-control" id="description" name="description">
+                </div>
+                <div class="mb-3">
+                    <label for="thumb" class="form-label">
+                        Url Image
+                    </label>
+                    <input type="text" class="form-control" id="thumb" name="thumb">
+                </div>
+                <div class="mb-3">
+                    <label for="price" class="form-label">
+                        Price
+                    </label>
+                    <input type="text" class="form-control" id="price" name="price">
+                </div>
+                <div class="mb-3">
+                    <label for="series" class="form-label">
+                        Series
+                    </label>
+                    <input type="text" class="form-control" id="series" name="series">
+                </div>
+                <div class="mb-3">
+                    <label for="sale_date" class="form-label">
+                        Sale Date
+                    </label>
+                    <input type="text" class="form-control" id="sale_date" name="sale_date">
+                </div>
+                <div class="mb-3">
+                    <label for="type" class="form-label">
+                        Type
+                    </label>
+                    <input type="text" class="form-control" id="type" name="type">
+                </div>
+                <div class="mb-3">
+                    <label for="artists" class="form-label">
+                        Artists
+                    </label>
+                    <input type="text" class="form-control" id="artists" name="artists">
+                </div>
+                <div class="mb-3">
+                    <label for="writers" class="form-label">
+                        Writers
+                    </label>
+                    <input type="text" class="form-control" id="writers" name="writers">
+                </div>
+                    <button type="submit" class="btn btn-primary">
+                        Add New Comic
+                    </button>
+            </form>
+        </div>
+    </div>
+</div>
+
+
+@endsection
