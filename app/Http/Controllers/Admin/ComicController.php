698514ef484a5ee02419f5bde34f0bb5e7efbc48
DanielMarella <marella.daniel96@gmail.com>
2023-07-25 19:02:31 +0200

    add a store function and a fill methods

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -37,7 +37,11 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+        $newComic = new Comic();
+        $newComic->fill($data);
+        $newComic->save();
+        return redirect()->route('admin.comic.show',$newComic->id);
     }
 
     /**
