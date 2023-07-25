261a1829a4d7b4f46da8880be3775dd9f64a985e
DanielMarella <marella.daniel96@gmail.com>
2023-07-25 17:00:29 +0200

    add my model to my controller

--- app/Http/Controllers/Guest/ComicController.php
+++ app/Http/Controllers/Guest/ComicController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comic;
 use Illuminate\Http\Request;
 
 class ComicController extends Controller
 {
     public function index(){
-        return view('guest.comic.index');
+        $comicList = Comic::All();
+        return view('guest.comic.index', compact('comicList'));
     }
 }
