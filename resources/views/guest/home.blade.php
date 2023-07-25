112601d931170c0136d5db5a6a93feeace51d4cd
DanielMarella <marella.daniel96@gmail.com>
2023-07-25 16:07:36 +0200

    add some html and style for better visualization of the page

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -4,8 +4,23 @@
 
 @section('main-content')
 
-<h1>
-    Homepage
-</h1>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="mb-5 mx-5">
+                Homepage
+            </h1>
+            <div class="img-wrapper mx-5">
+                <img src="https://i0.wp.com/www.meganerd.it/wp-content/uploads/2022/07/dc-comics-sdcc.jpg?fit=1665%2C1073&ssl=1" class="w-50 mb-5" alt="">
+            </div>
+            <p class="w-50 mx-5">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam ullam eos distinctio porro modi obcaecati impedit, cupiditate illum exercitationem commodi reiciendis molestias saepe vero, deleniti accusantium officia deserunt inventore ipsum!
+                Inventore eaque architecto, excepturi blanditiis voluptatum quo, aut vel optio consectetur mollitia adipisci itaque, amet quis earum quibusdam in officia reprehenderit soluta et? Inventore corrupti non id repellat laborum dolor.
+                Nobis, aperiam ratione ab provident modi cumque pariatur, totam necessitatibus perferendis ipsa tenetur cupiditate amet veritatis incidunt id in laboriosam. Nihil, autem eveniet. Numquam debitis inventore illum doloremque dignissimos voluptate.
+                Doloremque recusandae provident quae iure sint quidem atque, amet ipsum autem molestias non tempora sed quo praesentium quisquam deserunt illo eius excepturi odit vel laudantium veritatis ex asperiores iste! Saepe.
+            </p>
+        </div>
+    </div>
+</div>
 
 @endsection
