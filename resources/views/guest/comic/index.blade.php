9a863c5e25e552ba9631085351acede2a7c7daed
DanielMarella <marella.daniel96@gmail.com>
2023-07-25 17:00:56 +0200

    write on page every column of  my db

--- resources/views/guest/comic/index.blade.php
+++ resources/views/guest/comic/index.blade.php
@@ -8,4 +8,20 @@
     Comics list
 </h1>
 
+    <ul>
+        @foreach ($comicList as $comic)
+            <li class="mb-4">
+                {{ $comic->title}}
+                {{ $comic->description}}
+                {{ $comic->thumb}}
+                {{ $comic->price}}
+                {{ $comic->series}}
+                {{ $comic->sale_date}}
+                {{ $comic->type}}
+                {{ $comic->artists}}
+                {{ $comic->writers}}
+
+            </li>
+        @endforeach
+    </ul>
 @endsection
