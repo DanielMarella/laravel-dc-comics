3fa87e66ec5de12df13859e4339040a3da8b24f6
DanielMarella <marella.daniel96@gmail.com>
2023-07-25 17:48:46 +0200

    adding a table for show the results

---
+++ resources/views/admin/comics/index.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.app')
+
+@section('title', 'Admin Comics Index')
+
+@section('main-content')
+
+<div class="container">
+    
+    <h1 class="mb-5">
+        Comics Index
+    </h1>
+    <div class="row">
+        <div class="col-12">
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">
+                            id
+                        </th>
+                        <th scope="col">
+                            Title
+                        </th>
+                        <th scope="col">
+                            Price
+                        </th>
+                        <th scope="col">
+                            Series
+                        </th>
+                        <th scope="col">
+                            Sale_date
+                        </th>
+                        <th scope="col">
+                            Type
+                        </th>
+                        <th scope="col">
+                            Action
+                        </th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($comicList as $comic)
+                        
+                    <tr>
+                        <th scope="row">{{ $comic->id}}</th>
+                        <td>
+                            {{ $comic->title}}
+                        </td>
+                        <td>
+                            {{ $comic->price}}
+                        </td>
+                        <td>
+                            {{ $comic->series}}
+                        </td>
+                        <td>
+                            {{ $comic->sale_date}}
+                        </td>
+                        <td>
+                            {{ $comic->type}}
+                        </td>
+                        <td>
+                             <a class="btn btn-sm btn-primary">View</a>
+                        </td>
+
+
+                    </tr>
+                </tbody>   
+                @endforeach
+            </table>
+        </div>
+    </div>
+</div>
+@endsection
