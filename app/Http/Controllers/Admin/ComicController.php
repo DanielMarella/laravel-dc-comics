ebd72e32d6ba228d536339d7c4846028027c80a2
DanielMarella <marella.daniel96@gmail.com>
2023-07-26 16:45:46 +0200

    adding delete/destroy function

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -91,6 +91,8 @@ class ComicController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $comic = Comic::findOrFail($id);
+        $comic->delete();
+        return redirect()->route('admin.comics.index');
     }
 }
